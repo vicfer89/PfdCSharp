f0cdc2526b632138fb7e3512b094072e05b30d8b
vfernandezfe <vfernandezfe@oesia.com>
2024-01-30 08:08:14 +0100

    Version inicial funcional

---
+++ HorizonteArtificial/Controles/ucPFD.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace HorizonteArtificial.Controles
+{
+    /// <summary>
+    /// Interaction logic for ucPFD.xaml
+    /// </summary>
+    public partial class ucPFD : UserControl
+    {
+
+        private double _PitchAngle;
+        private double _RollAngle;
+ 
+        [Description("Angulo de Pitch"), Category("Condiciones Iniciales")]
+        public double PitchAngle
+        {                  
+            get { return _PitchAngle; }
+            set
+            {
+                _PitchAngle = value;
+                Background.RenderTransform = ApplyTransform();
+            }
+        }
+
+        [Description("Angulo de Roll"), Category("Condiciones Iniciales")]
+        public double RollAngle
+        {
+            get { return _RollAngle; }
+            set
+            {
+                _RollAngle = value;
+                Background.RenderTransform = ApplyTransform();
+            }
+        }
+
+        private TransformGroup ApplyTransform()
+        {
+            TransformGroup myTransformGroup = new TransformGroup();
+            myTransformGroup.Children.Add(new TranslateTransform(0, TransformAngleToSizePitch(_PitchAngle)));
+            myTransformGroup.Children.Add(new RotateTransform(_RollAngle));
+            return myTransformGroup;
+        }
+
+
+        public ucPFD()
+        {
+            InitializeComponent();
+        }
+
+        private double TransformAngleToSizePitch(double angle)
+        {
+            angle = (angle > 90.0) ? 90.0 : angle;
+            angle = (angle < -90.0) ? -90.0 : angle;            
+            return (angle * 250.0) / 90.0;
+        }
+    }
+}
---
+++ HorizonteArtificial/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace HorizonteArtificial
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Roll_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            PFD.RollAngle = Roll.Value;
+        }
+
+        private void Pitch_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            PFD.PitchAngle = Pitch.Value;
+        }
+    }
+}
